bd88028810d43bb9f8a119aeb5a691dba5c0e825
Maurilio Tadeu Dos Santos <maurilio.santos@neoris.com>
2024-02-08 21:12:09 -0300

    arquivo jenkins.xml atualizado

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,29 +1,17 @@
+
 pipeline {
     agent any
     
-    stages {
-        stage('Run UFT Tests') {
-            steps {
-                // Comando para executar os testes UFT
-                // Por exemplo, você pode chamar o executável do UFT e passar os testes a serem executados
-                bat '"C:\\Program Files (x86)\\OpenText\\UFT One\\bin\\UFT.exe"' -test '"C:\\Users\\maurilio.santos\\OneDrive - NEORIS\\Área de Trabalho\\Cenarios-de-tests\\ValidarCompraFlight"'
-            }
-        }
+    environment {
+        UFT_HOME = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OpenText\OpenText UFT One\Tools'
+        UFT_TEST = 'C:\Users\maurilio.santos\OneDrive - NEORIS\Área de Trabalho\Cenarios-de-tests'
     }
     
-    post {
-        always {
-            // Após a execução dos testes, gerar o arquivo XML de resultados
-            script {
-                // Aqui você precisa ajustar o caminho e o nome do arquivo de resultado conforme necessário
-               def resultFile = '"C:\\Users\\maurilio.santos\\OneDrive - NEORIS\\Área de Trabalho\\Cenarios-de-tests"'
-                
-                // Gere o arquivo XML com base nos resultados dos testes
-                                 
-                // Arquivo XML de resultados gerado
-               // <echo "Arquivo XML de resultados gerado em ${resultFile}"/>
+    stages {
+        stage('Preparar Ambiente') {
+            steps {
+                bat "\"${UFT_HOME}\\bin\\uft.bat\" -run -TestPath \"${UFT_TEST}\""
             }
         }
     }
 }
-
